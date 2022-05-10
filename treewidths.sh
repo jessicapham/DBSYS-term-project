fcfd0b45b4c3fb79c8ad658563dc0d0f2e95bbf1
Jessica Pham <jessica.pham@utexas.edu>
2022-05-10 09:35:14 -0500

    print results to console in md format

--- treewidths.sh
+++ treewidths.sh
@@ -10,6 +10,13 @@ docker run -it -v $PWD:/home db-project sh -c "cd ./Triangulator/ && make clean 
                                                 && echo 'Running benchmarks for Cypher queries...' \
                                                 && ./run_cypher.sh lsqb \
                                                 && ./run_cypher.sh ldbc \
-                                                && echo 'DONE'"
+                                                && echo 'DONE' \
+                                                && echo '------ BENCHMARK RESULTS FOR CYPHER QUERIES -----' \
+                                                && python3 res2md.py project_cypher/results_ldbc.txt \
+                                                && python3 res2md.py project_cypher/results_lsqb.txt \
+                                                && echo '------ BENCHMARK RESULTS FOR SQL QUERIES -----' \
+                                                && python3 res2md.py project_sql/results_lsqb.txt \
+                                                && python3 res2md.py project_sql/results_job.txt \
+                                                && python3 res2md.py project_sql/results_tpc-h.txt"
 
                                                 
